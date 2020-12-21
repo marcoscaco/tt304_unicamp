d7e16c528fd5149d5eda1f4c0979ac924bab8653
Marcos Antonio Cordovil de Oliveira <marcoantonio.caco@gmail.com>
2020-12-21 14:29:42 -0300

    Corrigido script inicial

--- infraestrutura/compile.sh
+++ infraestrutura/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 cd /trabalhoProg/app
-gcc main.c -w -l phthread -o main
+gcc main.c -w -l pthread -o main
 chmod 777 main
--- infraestrutura/get_git.sh
+++ infraestrutura/get_git.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 cd /
 git clone https://github.com/marcoscaco/tt304_unicamp.git /trabalhoProg
-chmod 777 -r /trabalhoProg
+chmod -R 777 /trabalhoProg
